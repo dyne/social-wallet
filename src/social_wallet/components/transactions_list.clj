f70c2ba03842c624c480548d4ec413af2088454a
bernini <bernini@inventati.org>
2019-06-30 17:25:12 +0200

    added filter per tags on transactions list (pure CSS)

--- src/social_wallet/components/transactions_list.clj
+++ src/social_wallet/components/transactions_list.clj
@@ -4,29 +4,47 @@
    [social-wallet.swapi :as swapi])
   )
 
+
+
 (defn transactions [account swapi-params query-params uri]
   (let [response (swapi/list-transactions swapi-params (cond-> {}
                                                          query-params (merge query-params)
                                                          account (assoc :account (:email account))))
         transactions (:transactions response)
-        total (:total-count response)]
-    [:div
-     [:table.func--transactions-page--table.table.table-striped
-      [:thead
-       [:tr
+        total (:total-count response)
+        tags (into [] (set (filter #(> (count %) 0)
+                                   (reduce into []
+                                           (map #(:tags %) (:transactions response))))))]
+    [:div.filter
+     [:input.filter-tag {:hidden true :checked true :name "filter-radio" :type "radio" :id "tag-0"}]
+     (for [t tags]
+       [:input.filter-tag {:hidden true :name "filter-radio" :type "radio" :id (str "tag-" (inc (.indexOf tags t)))}])
+
+     [:div.filter-nav
+      [:label.chip {:for "tag-0"} "All"]
+      (for [t tags]
+        [:label.chip {:for (str "tag-" (inc (.indexOf tags t)))} t])]
+
+     [:div.filter-body
+      [:table.func--transactions-page--table.table.table-striped
+       [:thead
+        [:tr
         ;; TODO: from transation
-        [:th "From"]
-        [:th "To"]
-        [:th "Amount"]
-        [:th "Time"]
-        [:th "Tags"]]]
-      [:tbody
-       (doall (for [t transactions]
-                [:tr
-                 [:td (:from-id t)]
-                 [:td (:to-id t)]
-                 [:td (:amount-text t)]
-                 [:td (:timestamp t)]
-                 [:td (interpose ", " (:tags t))]]))]]
+         [:th "From"]
+         [:th "To"]
+         [:th "Amount"]
+         [:th "Time"]
+         [:th "Tags"]]]
+       [:tbody
+        (doall (for [t transactions]
+                 [:tr.filter-item 
+                  {:data-tag (str "tag-" (inc (.indexOf tags (first (:tags t)))))}
+                  
+                  
+                  [:td (:from-id t)]
+                  [:td (:to-id t)]
+                  [:td (:amount-text t)]
+                  [:td (:timestamp t)]
+                  [:td (for [tag (:tags t)] (if (> (count tag) 0) [:div.chip tag] [:div]))]]))]]]
      (when (= uri "/transactions")
        (pagination total (or (:page query-params) 1) uri))]))
