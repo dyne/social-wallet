4c83daf3896a07ae1bf71460260120843515f0b1
Aspasia Beneti <aspasia.beneti@gmail.com>
2019-09-19 15:44:05 +0200

    :bookmark: Update version to stable release
    First stable release of the social wallet UI.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
 
 ;; If you modify this Program, or any covered work, by linking or combining it with any library (or a modified version of that library), containing parts covered by the terms of EPL v 1.0, the licensors of this Program grant you additional permission to convey the resulting work. Your modified version must prominently offer all users interacting with it remotely through a computer network (if your version supports such interaction) an opportunity to receive the Corresponding Source of your version by providing access to the Corresponding Source from a network server at no charge, through some standard or customary means of facilitating copying of software. Corresponding Source for a non-source form of such a combination shall include the source code for the parts of the libraries (dependencies) covered by the terms of EPL v 1.0 used as well as that of the covered work.
 
-(defproject social-wallet "0.1.0-SNAPSHOT"
+(defproject social-wallet "1.0"
   :description "Basic compojure based authenticated website"
   :url "http://dyne.org"
   :min-lein-version "2.0.0"
